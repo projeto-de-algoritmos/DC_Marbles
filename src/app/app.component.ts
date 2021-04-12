3998d847877c49298367f932ffcb01cbdb38f7d5
Calebe Rios <calebersmendes@gmail.com>
2021-04-12 19:35:33 -0300

    Creates view for puzzle 	- Option to difficult 	- Get numbers from marbles
    Co-authored-by: Saleh Kader <saleh.nazih.dev@gmail.com>v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { timer, Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { Marbles } from '../algorithm/Marbles';
+import { MeenaSelect } from '../algorithm/MeenaSelect';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +11,80 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  string1FormControl = new FormControl('', [Validators.required,]);
-  string2FormControl = new FormControl('', [Validators.required,]);
-  result = '';
+  answer = new FormControl(0, [Validators.required,]);
+  difficultValues: string[] = ['hard', 'medium', 'easy'];
+  selectedValue: string = 'easy';
+  numbers: number[] = [];
+  subscribeTimer: any = 0;
+  timeLeft: number = 60;
+  marbles: Marbles = new Marbles();
+  init: boolean = false;
+  response?: MeenaSelect;
+  source = timer(1000, 1000);
+  subscription?: Subscription;
+
+  start() {
+    this.numbers = this.marbles.difficultMarbles(this.selectedValue);
+    this.response = this.marbles.meenaMarbleSelectedToConsult(this.numbers);
+
+    console.log(this.response);
+
+    Swal.fire({
+      // icon: 'error',
+      title: 'Boa sorte',
+      text: `Adivinhe a posição do número: ${this.response.selectMarble}`
+    }).then((result) => {
+      this.init = true
+      this.observableTimer()
+    });
+  }
+
+  observableTimer() {
+    this.subscribeTimer = this.timeLeft;
+    this.subscription = this.source.subscribe(val => {
+      if (this.subscribeTimer <= 0) {
+        this.subscribeTimer = 0
+      } else {
+        console.log(val, '-');
+        this.subscribeTimer = this.timeLeft - val;
+      }
+    });
+  }
+
+  cancelTimer() {
+    this.subscription?.unsubscribe()
+  }
+
+  confirmwAnswer() {
+    console.log(this.answer.value);
+    this.init = false;
+    this.cancelTimer();
+    if (this.response?.position == undefined) {
+      if (this.answer.value == 0) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Parabens!!!',
+          text: 'Você acertou!'
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Não foi dessa vez',
+          text: 'Você não acertou a posição correta.'
+        });
+      }
+    } else if (this.answer.value == this.response.position) {
+      Swal.fire({
+          icon: 'success',
+          title: 'Parabens!!!',
+          text: 'Você acertou!'
+        });
+    } else {
+      Swal.fire({
+          icon: 'error',
+          title: 'Não foi dessa vez',
+          text: 'Você não acertou a posição correta.'
+        });
+    }
+  }
 }
